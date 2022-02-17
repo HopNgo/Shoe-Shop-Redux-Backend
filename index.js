c4e706cb8971975a806d0943b5bdaf3deb408654
HopNgo <ngoxuanhop254@gmail.com>
2022-02-17 15:37:30 +0700

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import commentRouter from './router/comment.js';
 dotenv.config();
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb' }));
