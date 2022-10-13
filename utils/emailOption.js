9bc0f819ea10d3da1ce682757157da1863120b0c
HopNgo <ngoxuanhop254@gmail.com>
2022-10-13 10:01:47 +0700

    add Oauth for send mail by nodemailer module

--- utils/emailOption.js
+++ utils/emailOption.js
@@ -1,31 +1,46 @@
 import nodemailer from "nodemailer";
+import { OAuth2Client } from "google-auth-library";
 
-const adminEmail = "nhom7shopgiay@gmail.com";
-const adminPassword = "tytqbnfnhmyowvyo";
-const mailPort = 587;
+const sendMail = async (emailCustomer, subject, htmlContent) => {
+  const GOOGLE_MAILER_CLIENT_ID = process.env.GOOGLE_MAILER_CLIENT_ID;
+  const GOOGLE_MAILER_CLIENT_SECRET = process.env.GOOGLE_MAILER_CLIENT_SECRET;
+  const GOOGLE_MAILER_REFRESH_TOKEN = process.env.GOOGLE_MAILER_REFRESH_TOKEN;
+  const ADMIN_EMAIL_ADDRESS = process.env.ADMIN_EMAIL_ADDRESS;
 
-//amgdaolrkliusuim
+  const myOAuth2Client = new OAuth2Client(
+    GOOGLE_MAILER_CLIENT_ID,
+    GOOGLE_MAILER_CLIENT_SECRET
+  );
 
-const sendMail = (emailCustomer, subject, htmlContent) => {
+  myOAuth2Client.setCredentials({
+    refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
+  });
+
+  if (!emailCustomer || !subject || !htmlContent)
+    throw new Error("Please provide email, subject and content!");
+
+  const myAccessTokenObject = await myOAuth2Client.getAccessToken();
+
+  const myAccessToken = myAccessTokenObject?.token;
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    port: mailPort,
-    secure: false,
     auth: {
-      user: adminEmail,
-      pass: adminPassword,
-    },
-    tls: {
-      rejectUnauthorized: false,
+      type: "OAuth2",
+      user: ADMIN_EMAIL_ADDRESS,
+      clientId: GOOGLE_MAILER_CLIENT_ID,
+      clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
+      refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
+      accessToken: myAccessToken,
     },
   });
   const mailOptions = {
-    from: `"Thông báo đặt hàng " <${adminEmail}>`,
-    to: [adminEmail, emailCustomer].toString(),
+    from: `"Thông báo đặt hàng từ SHOE STORE>`,
+    to: [ADMIN_EMAIL_ADDRESS, emailCustomer].toString(),
     subject: subject,
     html: htmlContent,
   };
-  return transporter.sendMail(mailOptions);
+  return await transporter.sendMail(mailOptions);
 };
 
 export default sendMail;
